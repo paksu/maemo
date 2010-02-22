d153dc25828896032940d80c3926ded2a3f8f285
Your Name <you@example.com>
2010-02-22 13:39:32 +0200

    util

---
+++ bogui/util.cpp
@@ -0,0 +1,27 @@
+#include "util.h"
+
+#include <QtGlobal>
+#include <QFile>
+#include <math.h>
+
+const qreal acc_expfilter_delta = 0.2;
+
+qreal read_acc()
+{
+    static qreal acc_last = 0.0;
+    static int acc_cache = 0;
+
+    qreal ans;
+    QByteArray arr;
+    QFile f("/sys/class/i2c-adapter/i2c-3/3-001d/coord");
+    if (f.open(QIODevice::ReadOnly)) {
+        arr = f.readLine(16);
+        f.close();
+        ans = -arr.left(arr.indexOf(' ')).toInt();
+    } else {
+        ans = ::sin((double)acc_cache++ / 100)*400;
+    }
+    ans = ans * acc_expfilter_delta + acc_last * (1.0 - acc_expfilter_delta);
+    acc_last = ans;
+    return ans;
+}
---
+++ bogui/util.h
@@ -0,0 +1,8 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <QtGlobal>
+
+qreal read_acc();
+
+#endif // UTIL_H
